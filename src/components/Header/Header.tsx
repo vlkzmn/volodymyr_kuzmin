e90063d08441e5e34e25576dfd59d7bfa88a638b
Volodymyr Kuzmin <vlkzmn@gmail.com>
2023-12-30 22:56:07 +0200

    fixed lazy loading

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,7 +14,6 @@ export const Header: React.FC = () => {
                 src="img/logo.svg"
                 className="header__logo-image"
                 alt="Volodymyr Kuzmin | Logo"
-                loading="lazy"
               />
             </a>
           </div>
